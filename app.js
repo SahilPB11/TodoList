13d04bd626f4e9e93fb248e9bc01db3a8e4c55f4
Sahil <garg07825@gmail.com>
2023-07-16 07:09:48 +0530

    logic completed

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views')
 app.use(bodyParser.urlencoded({extended:true}))
-
+let newItemsList = [];
 // get a value
 app.get('/', (req, res) => {
     let options = {weekday : "long", year : 'numeric', month: 'long', day: 'numeric'};
     const today = new Date();
     const day = today.toLocaleDateString("en-US", options);
-    res.render("list", {kindofday : day});
+    res.render("list", {kindofday : day, newItemsList : newItemsList});
 })
 
 app.post('/', (req, res) => {
     let newItem = req.body.newItem;
-    res.send(newItem)
+    newItemsList.push(newItem);
+    res.redirect('/');
 })
 
 app.listen(5000, () => {
