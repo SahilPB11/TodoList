0b51128ca74ac7188d9e3df7fcce97ce52b4e001
Sahil <garg07825@gmail.com>
2023-07-16 06:15:56 +0530

    added the ejs

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const app = express();
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
 
 // get a value
 app.get('/', (req, res) => {
-    res.send("mango khayeg")
+    let options = {weekday : "long", yaer : 'numeric', month: 'long', day: 'numeric'};
+    const today = new Date();
+    const day = today.toLocaleDateString("en-US", options);
+    res.render("list", {kindofday : day});
 })
 
 app.listen(5000, () => {
