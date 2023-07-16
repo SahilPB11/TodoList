d193deac0354d163daf45fd82e3d49482c04f126
Sahil <garg07825@gmail.com>
2023-07-16 06:53:26 +0530

    added the post method

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const app = express();
 app.set('view engine', 'ejs');
+app.set('views', './views')
 app.use(bodyParser.urlencoded({extended:true}))
 
 // get a value
 app.get('/', (req, res) => {
-    let options = {weekday : "long", yaer : 'numeric', month: 'long', day: 'numeric'};
+    let options = {weekday : "long", year : 'numeric', month: 'long', day: 'numeric'};
     const today = new Date();
     const day = today.toLocaleDateString("en-US", options);
     res.render("list", {kindofday : day});
+})
+
+app.post('/', (req, res) => {
+    let newItem = req.body.newItem;
+    res.send(newItem)
 })
 
 app.listen(5000, () => {
